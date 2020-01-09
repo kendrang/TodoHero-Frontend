48ef1fe0e9c75aea2fa6d06c50ccf3a524b9a05b
kendrang <kendrang@users.noreply.github.com>
2020-01-09 11:39:50 -0500

    adding basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './Auth/register/register.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { SuccessfulRegisterComponent } from './Auth/successful-register/successful-register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    SuccessfulRegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
